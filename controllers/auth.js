f8c0cafcfc0e3a51b58bd1804ef939e6b439b689
Abi0l9 <cryptog3ms@gmail.com>
2023-08-05 09:00:33 +0100

    fixed logging in issue

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,8 +36,6 @@ router
       "email",
       "phoneNumber",
       "role",
-      "gender",
-      "birthday",
       "password",
     ];
 
--- index.js
+++ index.js
@@ -10,10 +10,10 @@ require("./db");
 app.use(express.json());
 app.use(cors());
 app.use(middleware.requestLogger);
-app.use(middleware.tokenExtractor);
 
 app.get("", (request, response) => response.send("Welcome home"));
 app.use("/api/auth", authRouter);
+app.use(middleware.tokenExtractor);
 app.use(middleware.userExtractor);
 app.use("/api/users/", usersRouter);
 
